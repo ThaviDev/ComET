fec162c5997e60522f97e490a3bc59b58b0ebb9d
ElDeividDev <38382030+ThaviDev@users.noreply.github.com>
2025-01-07 21:42:48 -0600

    Visual Scripting Test Successfull
    + Small Sprite Pickup Changes

--- Assets/MyAssets/PickUps/CandyPickUp.cs
+++ Assets/MyAssets/PickUps/CandyPickUp.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class CandyPickUp : PickUp
 {
+    /*
     [SerializeField] Sprite _sprite;
     protected override void Start()
     {
         base.Start();
         base._sr.sprite = _sprite;
     }
+    */
 
     protected override void WasPickedUp(PlayerStats _pyr)
     {
